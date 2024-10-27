539d25fa489effc03c2c02ade329df42aece30de
koroush bagheri <kooroshbagheri97director@gmail.com>
2024-10-27 23:29:07 +0330

    flex-test

--- src/components/MainPosts/MainPosts.jsx
+++ src/components/MainPosts/MainPosts.jsx
@@ -6,20 +6,7 @@ import Avatar from '@mui/material/Avatar';
 export default function MainPosts() {
   return (
     <Box className="posts_container">
-        <Box className="postes_items banafsh" >
-        <Box className="avata-text">
-        <Avatar className='avatar-text-avatar' alt="Remy Sharp" src="" />
-            <span className='avatar-info'>avatar info</span>
-        </Box>
-        <Box className="sperad-box">
-            <span className='sperad-box-span'>...</span>
-        </Box>
-        </Box>
-        <Box className="postes_items sabz" > 
-            <Box className='media-post'>thats  my media</Box>
-         </Box>
-        <Box className="postes_items red" > icones </Box>
-        <Box className="postes_items zard" >texts</Box>
+      
     </Box>
   )
 }
