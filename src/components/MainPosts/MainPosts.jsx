83dd4d0ac14b6fcc1d1e4e8bf7dbf645bfca73d5
koroush bagheri <kooroshbagheri97director@gmail.com>
2024-10-27 23:51:36 +0330

    first test of flex was done and ok

--- src/components/MainPosts/MainPosts.jsx
+++ src/components/MainPosts/MainPosts.jsx
@@ -6,7 +6,16 @@ import Avatar from '@mui/material/Avatar';
 export default function MainPosts() {
   return (
     <Box className="posts_container">
-      
+     
+      <Box className="avatar-info-spred-box">
+        <Box className="avatar-info" >
+        <Avatar className='avatarinfo-avatar' alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <p className='avatar-info-p'>avatar-info</p>
+        </Box>
+        <Box className="spred" > <span className='spred-span'>...</span> </Box>
+      </Box>
+
+
     </Box>
   )
 }
