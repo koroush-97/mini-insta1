b545fc664c72ba227722bfde08e2c6e2411ae097
koroush bagheri <kooroshbagheri97director@gmail.com>
2024-10-28 20:02:21 +0330

    profile page working

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './Profile.css'
 import { Box } from '@mui/material'
 import Topbarprofile from '../../components/Topbarprofile/Topbarprofile'
+import ProfileMain from '../../components/profileMain/ProfileMain'
+
 
 export default function Profile() {
   return (
     <div className='Profile-container'>
       <Box className="profile-container-page">
         <Topbarprofile />
+        <ProfileMain />
       </Box>
     </div>
   )
