a5cd92f8318a5e836cb2f7074a473fdb70bf72a2
koroush bagheri <kooroshbagheri97director@gmail.com>
2024-11-11 17:13:42 +0330

    all main page was done

--- src/components/slidercerclies/Slidercercles.jsx
+++ src/components/slidercerclies/Slidercercles.jsx
@@ -1,49 +1,53 @@
 
-import React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import './slidercercles.css';
+import React, { useEffect, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import "./slidercercles.css";
 
 export default function Slidercercles() {
+  const persionInfoPics = async () => {
+    try {
+      const res = await fetch("https://randomuser.me/api/?results=30");
+      const result = await res.json();
+      setSliderspics(result.results); 
+    } catch (error) {
+      console.log("thats an error for sliderscyrcles", error);
+    }
+  };
+
+  useEffect(() => {
+    persionInfoPics(); 
+  }, []);
+
+  const [sliderspics, setSliderspics] = useState([]);
+
   return (
-    <div className='containerSlidercercles'>
-      <Stack direction="row" spacing={1} className='stack'>
-        <Box sx={{ display : 'flex', flexDirection: "column"  , alignItems: "center" , justifyContent: "center" }}>
-        <Box className="avatarWrapper">
-        <div className="avatarContent">
-          <Avatar alt="images" src="" />
-          </div>
-        </Box>
-        <span>person info</span>
-        </Box>
-       
-        <Box sx={{ display : 'flex', flexDirection: "column"  , alignItems: "center" , justifyContent: "center" }}>
-        <Box className="avatarWrapper">
-        <div className="avatarContent">
-          <Avatar alt="images" src="" />
-          </div>
-        </Box>
-        <span>person info</span>
-        </Box>
-       
-        <Box sx={{ display : 'flex', flexDirection: "column"  , alignItems: "center" , justifyContent: "center" }}>
-        <Box className="avatarWrapper">
-        <div className="avatarContent">
-          <Avatar alt="images" src="" />
-          </div>
-        </Box>
-        <span>person info</span>
-        </Box>
-       
-        <Box sx={{ display : 'flex', flexDirection: "column"  , alignItems: "center" , justifyContent: "center" }}>
-        <Box className="avatarWrapper">
-        <div className="avatarContent">
-          <Avatar alt="images" src="" />
-          </div>
-        </Box>
-        <span>person info</span>
-        </Box>
+    <div className="containerSlidercercles">
+      <Stack direction="row" spacing={3} className="stack">
+        {sliderspics.length ? (
+          sliderspics.map((pic, index) => (
+            <Box
+              key={index} 
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Box className="avatarWrapper">
+                <div className="avatarContent">
+                  <Avatar alt="images" src={pic.picture.large} />
+                </div>
+              </Box>
+              <span>{pic.name.first}</span>
+
+            </Box>
+          ))
+        ) : (
+          "profile ها در حال بارگیری هستند ..."
+        )}
       </Stack>
     </div>
   );
